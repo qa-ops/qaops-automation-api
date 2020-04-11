02723bc2ebd3da371bb4c3edf20ca3cbd13dbcf8
Rafael Lima <rafael.lima@transferwise.com>
2020-04-11 14:59:56 +0100

    Create test for creating the user

--- src/test/java/qaops/automation/api/AppTest.java
+++ src/test/java/qaops/automation/api/AppTest.java
@@ -3,7 +3,10 @@
  */
 package qaops.automation.api;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
@@ -12,6 +15,12 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 
 public class AppTest {
+
+    @BeforeClass
+    public static void setup() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+
 
     @Test
     public void testListUserMetadata() {
@@ -23,4 +32,15 @@ public class AppTest {
             body("data", is(notNullValue()));
     }
 
+    @Test
+    public void testSuccessfullyCreateaUser() {
+        given().
+            contentType(ContentType.JSON).
+            body("{\"name\": \"rafael\", \"job\": \"eng test\" }").
+        when().
+            post("https://reqres.in/api/user").
+        then().
+            statusCode(HttpStatus.SC_CREATED).
+            body("name", is("rafael"));
+    }
 }
