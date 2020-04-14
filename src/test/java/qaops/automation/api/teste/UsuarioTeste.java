54ef6b28270178ef37848e6bfe0ba7658d108a45
Rafael Lima <rafael.lima@transferwise.com>
2020-04-14 20:21:30 +0100

    Movendo o teste de usuario para o pacate teste

--- src/test/java/qaops/automation/api/teste/UsuarioTeste.java
+++ src/test/java/qaops/automation/api/teste/UsuarioTeste.java
@@ -1,4 +1,4 @@
-package qaops.automation.api;
+package qaops.automation.api.teste;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
@@ -6,23 +6,26 @@ import org.apache.http.HttpStatus;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.when;
+import static io.restassured.RestAssured.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 
-public class AppTest {
+public class UsuarioTeste {
 
     @BeforeClass
     public static void setup() {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        baseURI = "https://reqres.in";
+        basePath = "/api";
     }
 
 
     @Test
     public void testListUserMetadata() {
+        given().
+            params("page","2").
         when().
-            get("https://reqres.in/api/users?page=2").
+            get("/users").
         then().
             statusCode(HttpStatus.SC_OK).
             body("page", is(2)).
@@ -35,7 +38,7 @@ public class AppTest {
             contentType(ContentType.JSON).
             body("{\"name\": \"rafael\", \"job\": \"eng test\" }").
         when().
-            post("https://reqres.in/api/user").
+            post("/user").
         then().
             statusCode(HttpStatus.SC_CREATED).
             body("name", is("rafael"));
