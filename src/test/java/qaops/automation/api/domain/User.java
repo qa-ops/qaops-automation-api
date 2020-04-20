b13867a68221fa24b77f1aca53636614ee46c37d
Rafael Lima <rafael.lima@transferwise.com>
2020-04-20 22:18:38 +0100

    Add email to the user domain and create register test

--- src/test/java/qaops/automation/api/domain/User.java
+++ src/test/java/qaops/automation/api/domain/User.java
@@ -3,12 +3,14 @@ package qaops.automation.api.domain;
 public class User {
     private String name;
     private String job;
+    private String email;
 
     public User() {}
 
-    public User(String name, String job) {
+    public User(String name, String job, String email) {
         this.name = name;
         this.job = job;
+        this.email = email;
     }
 
     public String getName() {
@@ -17,5 +19,13 @@ public class User {
 
     public String getJob() {
         return job;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
