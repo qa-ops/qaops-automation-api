e1090808872eb1bde2ba50b45f6cde1522fc1a3a
Rafael Lima <rafael.lima@transferwise.com>
2020-04-23 21:53:01 +0100

    Add test to check that data items match per_page

--- src/test/java/qaops/automation/api/test/UserTest.java
+++ src/test/java/qaops/automation/api/test/UserTest.java
@@ -37,4 +37,33 @@ public class UserTest extends BaseTest {
             statusCode(HttpStatus.SC_CREATED).
             body("name", is("rafael"));
     }
+
+    @Test
+    public void testSizeOfItemsDisplayedAreTheSameAsPerPage() {
+
+        int expectedItemsPerPage = getExpectedItemsPerPage();
+
+        given().
+            params("page","2").
+        when().
+            get(LIST_USERS_ENDPOINT).
+        then().
+            statusCode(HttpStatus.SC_OK).
+            body(
+                "page", is(2),
+                "data.size()", is(expectedItemsPerPage ),
+                "data.findAll { it.avatar.startsWith('https://s3.amazonaws.com') }.size()", is(expectedItemsPerPage)
+            );
+    }
+
+    private int getExpectedItemsPerPage() {
+        return given().
+                    param("page", "2").
+                when().
+                    get(LIST_USERS_ENDPOINT).
+                then().
+                    statusCode(HttpStatus.SC_OK).
+                extract().
+                    path("per_page");
+    }
 }
