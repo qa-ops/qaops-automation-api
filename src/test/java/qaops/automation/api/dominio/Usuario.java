918cfc65e601b237f299c15557359b9f478376c4
Rafael Lima <rafael.lima@transferwise.com>
2020-04-27 20:00:04 +0100

    Adiciona teste de mostrar um usuario e usa deserializacao

--- src/test/java/qaops/automation/api/dominio/Usuario.java
+++ src/test/java/qaops/automation/api/dominio/Usuario.java
@@ -1,10 +1,19 @@
 package qaops.automation.api.dominio;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonSetter;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Usuario {
 
+    @JsonAlias("first_name")
     private String name;
     private String job;
     private String email;
+    @JsonAlias("last_name")
+    private String lastName;
 
     public Usuario() {}
 
@@ -28,5 +37,13 @@ public class Usuario {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
--- src/test/java/qaops/automation/api/teste/TesteUsuario.java
+++ src/test/java/qaops/automation/api/teste/TesteUsuario.java
@@ -5,13 +5,16 @@ import org.junit.Test;
 import qaops.automation.api.dominio.Usuario;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TesteUsuario extends TesteBase {
 
     private static final String LISTA_USUARIOS_ENDPOINT = "/users";
     private static final String CRIAR_USUARIO_ENDPOINT = "/user";
+    private static final String MOSTRAR_USUARIO_ENDPOINT = "/users/{userId}";
 
     @Test
     public void testeMostraPaginaEspecifica() {
@@ -54,6 +57,24 @@ public class TesteUsuario extends TesteBase {
                 "data.size()", is(perPageEsperado),
                 "data.findAll { it.avatar.startsWith('https://s3.amazonaws.com') }.size()", is(perPageEsperado)
             );
+    }
+
+    @Test
+    public void testeMostraUsuarioEspecifico() {
+        Usuario usuario = given().
+            pathParam("userId", 2).
+        when().
+            get(MOSTRAR_USUARIO_ENDPOINT).
+        then().
+            statusCode(HttpStatus.SC_OK).
+        extract().
+            body().jsonPath().getObject("data", Usuario.class);
+
+
+        assertThat(usuario.getEmail(), containsString("@reqres.in"));
+        assertThat(usuario.getName(), is("Janet"));
+        assertThat(usuario.getLastName(), is("Weaver"));
+
     }
 
     private int retornaPerPageEsperado(int page) {
