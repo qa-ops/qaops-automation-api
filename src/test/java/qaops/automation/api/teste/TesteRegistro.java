f77b47a73667e5d631310112b9c3e2298ff529c3
Rafael Lima <rafael.lima@transferwise.com>
2020-04-27 19:31:20 +0100

    Adiciona teste de login nao efetuado

--- src/test/java/qaops/automation/api/teste/TesteRegistro.java
+++ src/test/java/qaops/automation/api/teste/TesteRegistro.java
@@ -1,6 +1,11 @@
 package qaops.automation.api.teste;
 
+import io.restassured.RestAssured;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import org.apache.http.HttpClientConnection;
 import org.apache.http.HttpStatus;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import qaops.automation.api.dominio.Usuario;
 
@@ -10,6 +15,14 @@ import static org.hamcrest.CoreMatchers.is;
 public class TesteRegistro extends TesteBase {
 
     private static final String REGISTRA_USUARIO_ENDPOINT = "/register";
+    private static final String LOGIN_USUARIO_ENDPOINT = "/login";
+
+    @BeforeClass
+    public static void setupRegistro() {
+        RestAssured.responseSpecification = new ResponseSpecBuilder()
+                .expectStatusCode(HttpStatus.SC_BAD_REQUEST)
+                .build();
+    }
 
     @Test
     public void testeNaoEfetuaRegistroQuandoSenhaEstaFaltando() {
@@ -21,7 +34,20 @@ public class TesteRegistro extends TesteBase {
         when().
             post(REGISTRA_USUARIO_ENDPOINT).
         then().
-            statusCode(HttpStatus.SC_BAD_REQUEST).
+            body("error", is("Missing password"));
+    }
+
+    //Esse teste deveria estar no TesteLogin, porém esta aqui para mostrar a funcionalidade de múltiples setups e ResponseSpec
+    @Test
+    public void testeLoginNaoEfetuadoQuandoSenhaEstaFaltando() {
+        Usuario usuario = new Usuario();
+        usuario.setEmail("sydney@fife");
+
+        given().
+            body(usuario).
+        when().
+            post(LOGIN_USUARIO_ENDPOINT).
+        then().
             body("error", is("Missing password"));
     }
 
